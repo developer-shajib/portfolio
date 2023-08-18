3cfe7be294cb5da6bf88c3845ce0c4a42cf99582
shajib <sunhailshajib1@gmail.com>
2023-08-19 00:18:08 +0600

    update resume button

--- components/HomeBtn.js
+++ components/HomeBtn.js
@@ -7,9 +7,9 @@ const HomeBtn = () => {
     <div className='flex gap-4 z-10'>
       <Link
         target='_blank'
-        href={'https://drive.google.com/file/d/1B_LCAPc44vpMqOCkfJ-6ToVXFO1v8SCp/view?usp=sharing'}
+        href={'https://drive.google.com/drive/folders/15eix6GanmpJmBCkGZbnJ9NFx-vhWerTS?usp=drive_link'}
         className='flex gap-5 items-center justify-center px-6 py-3 rounded-lg bg-red text-base text-white shadow-inner hover:shadow-2xl'>
-        Download Resume <BiSolidDownload className='text-2xl' />
+        See Resume <FaRegEye className='text-2xl' />
       </Link>
       <Link
         href={'/project'}
