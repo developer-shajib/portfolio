7a8b9a4940392c2338494603787504985417a973
shajib <sunhailshajib1@gmail.com>
2023-09-08 11:17:52 +0600

    Resume button modify

--- components/HomeBtn.js
+++ components/HomeBtn.js
@@ -5,7 +5,7 @@ const HomeBtn = () => {
   return (
     <div className='flex gap-4 z-10'>
       <Link
-        // target='_blank'
+        target='_blank'
         download='resume.pdf'
         href='resume.pdf'
         className='flex gap-5 items-center justify-center px-6 py-3 rounded-lg bg-red text-base text-white shadow-inner hover:shadow-2xl'>
