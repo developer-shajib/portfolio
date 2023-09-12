8da61e6f54eb9f1e873ba4394d012bd6bfeb57b3
shajib <sunhailshajib1@gmail.com>
2023-09-12 10:39:26 +0600

    Project imagage modify

--- components/ProjectSlider.js
+++ components/ProjectSlider.js
@@ -1,23 +1,24 @@
 import Image from 'next/image.js';
 import Link from 'next/link.js';
 import Slider from 'react-slick';
-import project1 from '@/public/portfolioImg/Tours-and-Travel-Book1.png';
-import project2 from '@/public/portfolioImg/Client-react-portfiolo.png';
+import project1 from '@/public/portfolioImg/Client-react-portfiolo.png';
+import project2 from '@/public/portfolioImg/Car-Showcase.png';
 import project3 from '@/public/portfolioImg/developershajib-portfolio.png';
-import project4 from '@/public/portfolioImg/Car-Showcase.png';
+import project4 from '@/public/portfolioImg/Tours-and-Travel-Book1.png';
 import project5 from '@/public/portfolioImg/devSocial.png';
 
 const projectList = [
   {
-    title: 'Tours & Travels Booking using React js',
-    link: 'https://tours-and-travels-booking.netlify.app',
-    github: 'https://github.com/developer-shajib/Tours-and-Travel-Booking',
-    image: project1
-  },
-  {
     title: 'React Portfolio',
     link: 'https://devshajib-portfolio.netlify.app',
     github: 'https://github.com/developer-shajib/Portfolio-with-React/tree/main',
+    image: project1
+  },
+
+  {
+    title: 'Car Showcase & Search',
+    link: 'https://car-showcase-next13-drab.vercel.app',
+    github: 'https://github.com/developer-shajib/Car-Showcase-with-Next-13',
     image: project2
   },
   {
@@ -27,9 +28,9 @@ const projectList = [
     image: project3
   },
   {
-    title: 'Car Showcase & Search',
-    link: 'https://car-showcase-next13-drab.vercel.app',
-    github: 'https://github.com/developer-shajib/Car-Showcase-with-Next-13',
+    title: 'Tours & Travels Booking using React js',
+    link: 'https://tours-and-travels-booking.netlify.app',
+    github: 'https://github.com/developer-shajib/Tours-and-Travel-Booking',
     image: project4
   },
   {
@@ -55,7 +56,9 @@ const ProjectSlider = () => {
       className='z-50 '
       {...settings}>
       {[...projectList]?.reverse().map((item, index) => (
-        <div key={index}>
+        <div
+          className='relative w-full h-[350px]  overflow-hidden rounded-lg shadow-lg transform hover:scale-105 transition-transform'
+          key={index}>
           <Image
             src={item?.image}
             alt='Project Image'
@@ -63,47 +66,36 @@ const ProjectSlider = () => {
             width={100}
             height={100}
           />
+
+          <div className='absolute top-0 left-0 w-full h-full bg-[#00000091] bg-opacity-50 opacity-0 hover:opacity-100 flex items-center justify-center transition-opacity duration-300'>
+            <div className='text-white text-center flex flex-col'>
+              <h3 className='text-xl font-medium'>{item.title}</h3>
+
+              <div className='mt-4 flex space-x-2 justify-center '>
+                <Link
+                  target='_blank'
+                  href={item?.link}
+                  className='bg-slate-950 hover:bg-red text-white px-3 py-1 rounded-md transition-colors duration-300'>
+                  Link
+                </Link>
+                <Link
+                  target='_blank'
+                  href={item?.github}
+                  className='bg-slate-950  hover:bg-red text-white px-3 py-1 rounded-md transition-colors duration-300'>
+                  Github
+                </Link>
+                {item?.video && (
+                  <Link
+                    target='_blank'
+                    href={item?.video}
+                    className='bg-slate-950  hover:bg-red text-white px-3 py-1 rounded-md transition-colors duration-300'>
+                    Video
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-        // <div
-        //   className='relative w-full h-[350px]  overflow-hidden rounded-lg shadow-lg transform hover:scale-105 transition-transform'
-        //   key={index}>
-        //   <Image
-        //     src={item?.image}
-        //     alt='Project Image'
-        //     className=' w-full h-full'
-        //     width={100}
-        //     height={100}
-        //   />
-
-        //   <div className='absolute top-0 left-0 w-full h-full bg-[#00000091] bg-opacity-50 opacity-0 hover:opacity-100 flex items-center justify-center transition-opacity duration-300'>
-        //     <div className='text-white text-center flex flex-col'>
-        //       <h3 className='text-xl font-medium'>{item.title}</h3>
-
-        //       <div className='mt-4 flex space-x-2 justify-center '>
-        //         <Link
-        //           target='_blank'
-        //           href={item?.link}
-        //           className='bg-slate-950 hover:bg-red text-white px-3 py-1 rounded-md transition-colors duration-300'>
-        //           Link
-        //         </Link>
-        //         <Link
-        //           target='_blank'
-        //           href={item?.github}
-        //           className='bg-slate-950  hover:bg-red text-white px-3 py-1 rounded-md transition-colors duration-300'>
-        //           Github
-        //         </Link>
-        //         {item?.video && (
-        //           <Link
-        //             target='_blank'
-        //             href={item?.video}
-        //             className='bg-slate-950  hover:bg-red text-white px-3 py-1 rounded-md transition-colors duration-300'>
-        //             Video
-        //           </Link>
-        //         )}
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
       ))}
     </Slider>
   );
