8d9e1ee204a73aef6f6c17a6b8ba65466be55820
shajib <sunhailshajib1@gmail.com>
2023-09-12 10:34:30 +0600

    Project imagage modify

--- components/ProjectSlider.js
+++ components/ProjectSlider.js
@@ -1,38 +1,43 @@
 import Image from 'next/image.js';
 import Link from 'next/link.js';
 import Slider from 'react-slick';
+import project1 from '@/public/portfolioImg/Tours-and-Travel-Book1.png';
+import project2 from '@/public/portfolioImg/Client-react-portfiolo.png';
+import project3 from '@/public/portfolioImg/developershajib-portfolio.png';
+import project4 from '@/public/portfolioImg/Car-Showcase.png';
+import project5 from '@/public/portfolioImg/devSocial.png';
 
 const projectList = [
   {
     title: 'Tours & Travels Booking using React js',
     link: 'https://tours-and-travels-booking.netlify.app',
     github: 'https://github.com/developer-shajib/Tours-and-Travel-Booking',
-    image: 'https://res.cloudinary.com/do6dt1ot2/image/upload/v1694168140/Portfolio/Tours-and-Travel-Book_vzr6bg.png'
+    image: project1
   },
   {
     title: 'React Portfolio',
     link: 'https://devshajib-portfolio.netlify.app',
     github: 'https://github.com/developer-shajib/Portfolio-with-React/tree/main',
-    image: 'https://res.cloudinary.com/do6dt1ot2/image/upload/v1694168141/Portfolio/React-Portfolio_exdqdh.png'
+    image: project2
   },
   {
     title: 'My Portfolio using Next js',
     link: 'https://developershajib.vercel.app/',
     github: 'https://github.com/developer-shajib/portfolio',
-    image: 'https://res.cloudinary.com/do6dt1ot2/image/upload/v1694168141/Portfolio/developershajib-portfolio_rfhxnf.png'
+    image: project3
   },
   {
     title: 'Car Showcase & Search',
     link: 'https://car-showcase-next13-drab.vercel.app',
     github: 'https://github.com/developer-shajib/Car-Showcase-with-Next-13',
-    image: 'https://res.cloudinary.com/do6dt1ot2/image/upload/v1694168141/Portfolio/project4_awz7pn.png'
+    image: project4
   },
   {
     title: 'Social Media website',
     link: 'https://dev-social-five.vercel.app',
     github: 'https://github.com/developer-shajib/DevSocial.git',
     video: 'https://www.linkedin.com/posts/developer-shajib_uses-features-activity-7105382804879790080-v3Oj?utm_source=share&utm_medium=member_desktop',
-    image: 'https://res.cloudinary.com/do6dt1ot2/image/upload/v1694168150/Portfolio/devSocial_usph5j.png'
+    image: project5
   }
 ];
 
@@ -47,7 +52,7 @@ const ProjectSlider = () => {
   };
   return (
     <Slider
-      className=' '
+      className='z-50 '
       {...settings}>
       {[...projectList]?.reverse().map((item, index) => (
         <div key={index}>
